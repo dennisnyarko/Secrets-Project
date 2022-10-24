0737732dafab4cfd743c9db9151ce2fec5bdec88
Dennis Nyarko <dennis_nyarko@outlook.com>
2022-10-24 23:25:01 +0000

    connect mongoose & create schema

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function(req, res){
     res.render("home");
 });
