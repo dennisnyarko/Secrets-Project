8d5d592e20a862f3b972a0b9474709aa64dd443c
Dennis Nyarko <dennis_nyarko@outlook.com>
2022-10-29 22:14:10 +0000

    salting with bcrypt & login test

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
 
 
 const app = express();
@@ -44,7 +42,7 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-    bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
 
         const newUser = new User({
             email: req.body.username,
@@ -65,16 +63,21 @@ app.post("/register", function(req, res){
 
     app.post("/login", function(req, res){
         const username = req.body.username;
-        const password = md5(req.body.password);
+        const password = req.body.password;
 
         User.findOne({email: username}, function(err, foundUser){
             if (err) {
                 console.log(err);
             } else {
                 if (foundUser) {
-                    if (foundUser.password === password) {
-                        res.render("secrets");
-                    }
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        if (result === true){
+                            res.render("secrets");
+
+                        }
+                    });
+                        
+                    
                 }
             }
     });
